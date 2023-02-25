6bd0ee39d8eb6e5507ddbce502cfa671fd33f8e6
Jacky Tung <jackyyet@usc.edu>
2023-02-25 01:34:43 -0800

    slight update to Neda's logic in firstoreHelper

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -1,9 +1,9 @@
 import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { firestore } from "./firebase-setup";
 
-export async function writeToDB(goal) {
+export async function writeToDB(entry) {
   try {
-    const docRef = await addDoc(collection(firestore, "goals"), goal);
+    const docRef = await addDoc(collection(firestore, "entries"), entry);
     console.log(docRef.id);
   } catch (err) {
     console.log(err);
@@ -12,7 +12,7 @@ export async function writeToDB(goal) {
 
 export async function deleteFromDB(id) {
   try {
-    await deleteDoc(doc(firestore, "goals", id));
+    await deleteDoc(doc(firestore, "entries", id));
   } catch (err) {
     console.log(err);
   }
