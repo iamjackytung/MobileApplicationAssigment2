7867a6c419a36760f2bbad837ebb8fcbcbcb205f
Jacky Tung <jackyyet@usc.edu>
2023-02-25 01:21:14 -0800

    Firebase environment is officially set up and ready to go

---
+++ Firebase/firebase-setup.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import React from "react";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+} from "@env";
+
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
+
+// Initialize Firebase
+const App = initializeApp(firebaseConfig);
+export const firestore = getFirestore(App);
